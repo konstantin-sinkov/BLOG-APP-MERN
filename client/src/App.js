c2b8b83dee3020e297bc36288a144e9d39b564a5
konstantin-sinkov <sinkov.ks@gmail.com>
2023-06-26 13:05:18 +0300

    Setting up client - 0.1

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+    return (
+        <main>
+            <header>
+                <a href="" className="logo">My Blog</a>
+                <nav>
+                    <a href="">Login</a>
+                    <a href="">Register</a>
+                </nav>
+            </header>
+            <div className="post">
+                <div className="post-image">
+                    <img
+                    src="https://techcrunch.com/wp-content/uploads/2021/10/GettyImages-1249853438.jpg?w=730&crop=1"
+                    alt="poster"/>
+                </div>
+                <div className="post-text">
+                    <h2>This week in AI: Big tech bets billions on machine learning tools</h2>
+                    <p>Keeping up with an industry as fast-moving as AI is a tall order. So until an AI can do it for
+                        you, here’s a handy roundup of the last week’s stories in the world of machine learning, along
+                        with no...</p>
+                </div>
+            </div>
+            <div className="post">
+                <div className="post-image">
+                    <img
+                        src="https://techcrunch.com/wp-content/uploads/2021/10/GettyImages-1249853438.jpg?w=730&crop=1"
+                        alt="poster"/>
+                </div>
+                <div className="post-text">
+                    <h2>This week in AI: Big tech bets billions on machine learning tools</h2>
+                    <p>Keeping up with an industry as fast-moving as AI is a tall order. So until an AI can do it for
+                        you, here’s a handy roundup of the last week’s stories in the world of machine learning, along
+                        with no...</p>
+                </div>
+            </div>
+            <div className="post">
+                <div className="post-image">
+                    <img
+                        src="https://techcrunch.com/wp-content/uploads/2021/10/GettyImages-1249853438.jpg?w=730&crop=1"
+                        alt="poster"/>
+                </div>
+                <div className="post-text">
+                    <h2>This week in AI: Big tech bets billions on machine learning tools</h2>
+                    <p>Keeping up with an industry as fast-moving as AI is a tall order. So until an AI can do it for
+                        you, here’s a handy roundup of the last week’s stories in the world of machine learning, along
+                        with no...</p>
+                </div>
+            </div>
+        </main>
+    );
+}
+
+export default App;
