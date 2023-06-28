d30142552f635d9347cf278024269eae59c74593
konstantin-sinkov <sinkov.ks@gmail.com>
2023-06-29 00:57:21 +0300

    Setting up server - Login and RegisterPages - 0.4

--- client/src/pages/RegisterPage/RegisterPage.js
+++ client/src/pages/RegisterPage/RegisterPage.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const RegisterPage = () => {
+    const [userName, setUserName] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+    
+    const register = (e) => {
+        e.preventDefault();
+        
+    }
+    
     return (
-        <form className="register">
+        <form className="register" onSubmit={register}>
             <h1>Register page</h1>
-            <input type="text" placeholder="username"/>
-            <input type="password" placeholder="password"/>
+            <input type="text"
+                   placeholder="username"
+                   value={userName}
+                   onChange={event => setUserName(event.target.value)}/>
+            <input type="password"
+                   placeholder="password"
+                   value={userPassword}
+                   onChange={event => setUserPassword((event.target.value))}/>
             <button>Login</button>
         </form>
     );
